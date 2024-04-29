0878427d7dd2d6c30018a2f7c0657c6588d2fe74
tosename <brenosentose@gmail.com>
2024-04-28 22:51:41 -0300

    #5 feat: Adiciona controller e rotas de locais

--- src/controllers/LocaisController.js
+++ src/controllers/LocaisController.js
@@ -1,7 +1,58 @@
-const prismaClient = require("../database/PrismaClient.js");
+const prismaClient  = require("../database/PrismaClient.js");
 
-class LocaisController{
+class LocaisController {
+    async listarLocais (request, response) {
+        const locais = await prismaClient.local.findMany();
 
+        response.status(200).json(locais);
+    }
+
+    async criarLocal (request, response) {
+        const { nome, endereco, cidade, estado, pais } = request.body;
+        const local = await prismaClient.local.create({
+            data: {
+                nome: nome,
+                endereco: endereco,
+                cidade: cidade,
+                estado: estado,
+                pais: pais,
+            }
+        });
+
+        response.status(201).json(local);
+    }
+
+    async atualizarLocal (request, response) {
+        const { id } = request.params;
+        const { nome, endereco, cidade, estado, pais } = request.body;
+        const local = await prismaClient.local.update({
+            where: {
+                id: id
+            },
+            data: {
+                nome: nome,
+                endereco: endereco,
+                cidade: cidade,
+                estado: estado,
+                pais: pais,
+            }
+        });
+
+        response.status(200).json(local);
+    }
+
+    async deletarLocal (request, response) {
+        const { id } = request.params
+        const local = await prismaClient.local.delete({
+            where: {
+                id: id
+            }
+        });
+
+        response.status(204).send();
+    }
 }
+
+//
 
 module.exports = LocaisController;
